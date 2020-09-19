c091d32ee919355c2969fd787a7a2ad8b2bfff83
luis torres <tl224627@gmail.com>
2020-09-19 13:15:19 -0600

    add some do loops and function

--- Dice/Dice.vb
+++ Dice/Dice.vb
@@ -2,11 +2,32 @@
 'RCET0265
 'Fall 2020
 'Roll of the Dice
-'
+'https://github.com/LuisTorres0419/Dice.git
 Module Dice
 
     Sub Main()
+        Dim userInput As String
+        Dim randomNumber As Integer
+        Dim data(12) As Integer
+        Dim column, row As Integer
 
+        Console.WriteLine("Press enter to roll if you DARE!!")
+        Console.Read()
+
+        Do While userInput <> "Q"
+            randomNumber = getRandomNumber(1, 15)
+            Console.Write(randomNumber)
+            Console.Read()
+            Console.Clear()
+        Loop
     End Sub
 
+    Function getRandomNumber(ByVal minimum As Integer, ByVal Maximum As Integer) As Integer
+        Dim value As Single
+        Randomize()
+        value = CInt(Int((6 * Rnd()) + 1))
+        Console.Write(value)
+        Console.WriteLine(Int(value))
+        Return CInt((value))
+    End Function
 End Module
